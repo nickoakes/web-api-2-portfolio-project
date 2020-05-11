da9fef9243059c493400f7036c55b50bdc019108
Nicholas Oakes <32389274+nickoakes@users.noreply.github.com>
2020-05-11 18:16:44 +0100

    Basic authentication with client secret and client ID added.

--- web-api-2-portfolio-project/Controllers/UsersController.cs
+++ web-api-2-portfolio-project/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using Swashbuckle.Swagger.Annotations;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Web.Http;
 using web_api_2_portfolio_project.Shared;
 using web_api_2_portfolio_project.UsersMethods;
@@ -10,31 +12,68 @@ namespace web_api_2_portfolio_project.Controllers
     [RoutePrefix("api")]
     public class UsersController : ApiController
     {
+        private bool CheckClientSecret()
+        {
+            IEnumerable<string> headerValuesClientSecret;
+            IEnumerable<string> headerValuesClientID;
+
+            if (Request
+                .Headers
+                .TryGetValues("client-secret", out headerValuesClientSecret) &&
+                (Request
+                .Headers
+                .TryGetValues("client-id", out headerValuesClientID)))
+            {
+                if (string
+                    .Equals(headerValuesClientSecret.FirstOrDefault(), Config.GetClientSecret()) &&
+                    (string
+                    .Equals(headerValuesClientID.FirstOrDefault(), Config.GetClientID())))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         [Route("users")]
         [HttpPost]
         [SwaggerResponse(200, Description = "Success", Type = typeof(User))]
         public dynamic GetUser(UsersSearchRequest request)
         {
-            SearchByID searchByID = new SearchByID();
-
-            SearchByName searchByName = new SearchByName();
-
-            List<string> errors = new List<string>();
-
-            DBC dbc = DBC.DatabaseConnection();
-
-            if (!string.IsNullOrEmpty(request.UserID))
+            if (CheckClientSecret())
             {
-                return searchByID.SearchUsersByID(dbc, request.UserID, errors);
-            }
+                SearchByID searchByID = new SearchByID();
 
-            if (!string.IsNullOrWhiteSpace(request.FirstName) ||
-                !string.IsNullOrWhiteSpace(request.LastName))
+                SearchByName searchByName = new SearchByName();
+
+                List<string> errors = new List<string>();
+
+                DBC dbc = DBC.DatabaseConnection();
+
+                if (!string.IsNullOrEmpty(request.UserID))
+                {
+                    return searchByID.SearchUsersByID(dbc, request.UserID, errors);
+                }
+
+                if (!string.IsNullOrWhiteSpace(request.FirstName) ||
+                    !string.IsNullOrWhiteSpace(request.LastName))
+                {
+                    return searchByName.SearchUsersByName(dbc, request, errors);
+                }
+
+                return new User();
+            }
+            else
             {
-                return searchByName.SearchUsersByName(dbc, request, errors);
+                return new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
             }
-
-            return new User();
         }
     }
 }
---
+++ web-api-2-portfolio-project/Shared/Config.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace web_api_2_portfolio_project.Shared
+{
+    public class Config
+    {
+        public static string GetClientID()
+        {
+            return ConfigurationManager.AppSettings.Get("client-id");
+        }
+
+        public static string GetClientSecret()
+        {
+            return ConfigurationManager.AppSettings.Get("client-secret");
+        }
+    }
+}
